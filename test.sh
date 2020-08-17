ff55619b6a339d4256a917ab89b4aafec50da1c9
xuhuaren <xuhuaren@tencent.com>
2020-08-17 14:18:45 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python tools/infer/predict_system.py --image_dir="./doc/imgs/2.jpg" --det_model_dir="./inference/ch_det_r50_vd_db/"  --rec_model_dir="./inference/ch_rec_r34_vd_crnn_enhance/" 
+python tools/infer/predict_system.py --image_dir="./doc/imgs/2.jpg" --det_model_dir="./inference/ch_det_mv3_db"  --rec_model_dir="./inference/ch_rec_r34_vd_crnn_enhance/" 
